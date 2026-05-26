dab4e9d32063a588ec795dbb578503f844593f81
Carson Phoon <142515330+CarsonPhoon@users.noreply.github.com>
2026-05-26 14:50:16 +0800

    Fix indentation and remove extra closing div tag

--- src/App.tsx
+++ src/App.tsx
@@ -843,7 +843,7 @@ export default function App() {
           )}
         </AnimatePresence>
       </motion.div>
-
+    </div>
       {/* Developer Credits Widget */}
       <div className="relative z-10 mt-2">
         <motion.button
@@ -885,6 +885,5 @@ export default function App() {
           </AnimatePresence>
         </motion.button>
       </div>
-    </div>
   );
 }
